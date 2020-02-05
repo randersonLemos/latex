c5f884ddd95e37ba9a9daa8610b01b843b5a89fe
Randerson <randerson.lemos@gmail.com>
2020-02-05 07:04:40 -0300

    Generation of NPV graph accomplished.

--- npv01.py
+++ npv01.py
@@ -1,3 +1,6 @@
+TO_TIKZ = True
+
+
 def filter_xtikcs(code, xticks):
     xticks = list(map(str, xticks))
     xticks_str = 'xtick={{{}}}'.format(','.join(xticks))
@@ -5,7 +8,6 @@ def filter_xtikcs(code, xticks):
     end = code.find('}',bgn)
     code = code.replace(code[bgn:end+1],xticks_str)
     return code
-
 
 
 import matplotlib.pyplot as plt
@@ -29,18 +31,32 @@ fig, ax = plt.subplots()
 
 x = nDf2.df['Close Condition']
 y = nDf2.df['Npv']/1000000
-ax.plot(x, y, 'mo')
+ax.plot(x, y, 'C0o', markersize=4)
 #ax.set_xticks(range(0,100,5))
 
 ax.tick_params(axis="x", labelrotation=90)
-ax.set_title('Net present value')
+#ax.set_title('Net present value\\\\Production strategies w/ ON/OFF ICV\'s')
 ax.set_xlabel('(GOR [$sm^3/sm^3$]; WCUT [$sm^3/sm^3$])')
 ax.set_ylabel('Millions of dollars')
-#plt.show()
+ax.set_xlim(-2,102)
 
-code = tikzplotlib.get_tikz_code()
-code = filter_xtikcs(code, range(0,100,5))
-with open('./latex/npvf/tikz/graph01.tikz', 'w') as fh:
-    fh.write(code)
+if TO_TIKZ:
+    settings = {}
+    settings['textsize'] = 6
+    settings['figurewidth'] = '0.90\\textwidth'
+    settings['figureheight'] = '0.60\\textheight'
+    eap = []
+    eap.append('title={Net present value\\\\\\scriptsize{Production strategies w/ ON/OFF ICV\'s}}')
+    eap.append('title style={align=left, font=\\normalsize}')
+    eap.append('tick label style={font=\\scriptsize}')
+    eap.append('label style={font=\\scriptsize}')
+    eap.append('legend pos=north east')
+    settings['extra_axis_parameters'] = eap
+    settings['extra_tikzpicture_parameters'] = None
+    code = tikzplotlib.get_tikz_code(**settings)
+    code = filter_xtikcs(code, range(0,100,3))
+    with open('./latex/npvf/tikz/graph01.tikz', 'w') as fh: fh.write(code)
+else:
+    plt.show()
 
 
